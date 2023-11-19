555167a67f26ea625f6dfffe9c93e9e6e5f9ff3c
oskar <ooskar@gmx.de>
2023-11-19 12:31:17 +0100

    changed bcrypt dependency to bcryptjs dependency due to installation issues

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,7 +4,7 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { UsersService } from '../users/users.service';
 
 @Injectable()

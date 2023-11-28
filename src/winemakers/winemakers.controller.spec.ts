1b9ced0b414ae14da6c953ddeaf1b17375f59e18
oskar <ooskar@gmx.de>
2023-11-28 10:50:44 +0100

    fix tests

--- src/winemakers/winemakers.controller.spec.ts
+++ src/winemakers/winemakers.controller.spec.ts
@@ -1,18 +1,7 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { WinemakersController } from './winemakers.controller';
 
 describe('WinemakersController', () => {
-  let controller: WinemakersController;
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [WinemakersController],
-    }).compile();
-
-    controller = module.get<WinemakersController>(WinemakersController);
-  });
-
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(WinemakersController).toBeDefined();
   });
 });

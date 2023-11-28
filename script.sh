895f907b287c0d94bd117e9944eb901c6dc25b72
oskar <ooskar@gmx.de>
2023-11-28 10:25:25 +0100

    removed /auth/profile route

--- script.sh
+++ script.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-docker run \
-    -e POSTGRES_USER=vinopinions-api \
-    -e POSTGRES_PASSWORD=JfK9pC^2Uq4&sn \
-    -e POSTGRES_DB=vinopinions-db \
-    -e POSTGRES_PORT=5432 \
-    -p 5432:5432 \
-    postgres:latest
+docker run -d -e POSTGRES_USER=vinopinions-api -e POSTGRES_PASSWORD=JfK9pC^2Uq4 -e POSTGRES_DB=vinopinions-db -p 5433:5432 postgres:latest

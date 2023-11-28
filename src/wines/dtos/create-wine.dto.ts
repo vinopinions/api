c358a7b875b9e82eaa9aaf65f0be7380d4a4901a
oskar <ooskar@gmx.de>
2023-11-28 10:47:08 +0100

    updated CreateWineDto

--- src/wines/dtos/create-wine.dto.ts
+++ src/wines/dtos/create-wine.dto.ts
@@ -4,5 +4,7 @@ import { Wine } from '../entities/wine.entity';
 export class CreateWineDto extends PickType(Wine, [
   'name',
   'year',
+  'grapeVariety',
+  'heritage',
   'winemaker',
 ] as const) {}

710646b080dd291b4c116c53c9ee75702dd1df47
oskar <ooskar@gmx.de>
2023-11-28 11:08:37 +0100

    Added winemaker creation endpoint

--- src/winemakers/winemakers.controller.ts
+++ src/winemakers/winemakers.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { CreateWineMakerDto } from './dtos/create-winemaker.dto';
+import { WinemakersService } from './winemakers.service';
 
 @Controller('winemakers')
-export class WinemakersController {}
+export class WinemakersController {
+  constructor(private winemakersService: WinemakersService) {}
+
+  @HttpCode(HttpStatus.CREATED)
+  @Post()
+  create(@Body() createWinemakersDto: CreateWineMakerDto) {
+    return this.winemakersService.create(createWinemakersDto.name);
+  }
+}

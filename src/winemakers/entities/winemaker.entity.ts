5afb353cda53c3d2a41f68472d2afb07740085db
oskar <ooskar@gmx.de>
2023-11-28 10:53:33 +0100

    added winemaker entity

---
+++ src/winemakers/entities/winemaker.entity.ts
@@ -0,0 +1,41 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Winemaker {
+  @ApiProperty({
+    readOnly: true,
+    example: 'uuid',
+    description: 'uuid',
+    type: String,
+    format: 'uuid',
+  })
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ApiProperty({
+    example: 'Sina Mertz',
+    description: 'name of the winemaker',
+    type: String,
+  })
+  @Column({ unique: true })
+  username: string;
+
+  @ApiProperty({
+    readOnly: true,
+    example: new Date(),
+    description: 'createdAt',
+    type: Date,
+  })
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}

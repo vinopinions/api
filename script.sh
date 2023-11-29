0dc6e984142ffb49de63a566e6fdc7e347f3860a
oskar <ooskar@gmx.de>
2023-11-29 11:51:30 +0100

    dirty test fix

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -d -e POSTGRES_USER=vinopinions-api -e POSTGRES_PASSWORD=JfK9pC^2Uq4 -e POSTGRES_DB=vinopinions-db -p 5433:5432 postgres:latest
+docker run -d --name vinopinions-local-postgres -e POSTGRES_USER=vinopinions-api -e POSTGRES_PASSWORD=JfK9pC^2Uq4 -e POSTGRES_DB=vinopinions-db -p 5433:5432 postgres:latest

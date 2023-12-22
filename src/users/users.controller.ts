6d352a700d4a8f1779dbc3edd00480cbce29af50
oskar <ooskar@gmx.de>
2023-12-22 09:23:20 +0100

    added endpoints for users

---
+++ src/users/users.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { UsersService } from './users.service';
+
+@Controller('users')
+@ApiTags('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Get(':name')
+  findByName(@Param('name') name: string) {
+    return this.usersService.findOneByName(name);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+}

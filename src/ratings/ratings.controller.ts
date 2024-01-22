6b708c202bcf192508731896a845747ba6100564
oskar <ooskar@gmx.de>
2024-01-22 10:05:34 +0100

    Added ApiResponses to all ratingsController endpoints

--- src/ratings/ratings.controller.ts
+++ src/ratings/ratings.controller.ts
@@ -8,34 +8,71 @@ import {
   Param,
   Post,
 } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { CreateRatingDto } from './dtos/create-rating.dto';
+import { Rating } from './entities/rating.entity';
 import { RatingsService } from './ratings.service';
 
 @Controller('ratings')
+@ApiUnauthorizedResponse({
+  description: 'Not logged in',
+})
 export class RatingsController {
   constructor(private ratingsService: RatingsService) {}
 
   @HttpCode(HttpStatus.OK)
   @Get(':id')
-  findById(@Param('id') id: string) {
+  @ApiOkResponse({
+    description: 'Rating has been found',
+    type: Rating,
+  })
+  @ApiNotFoundResponse({
+    description: 'Rating has not been found',
+  })
+  findById(@Param('id') id: string): Promise<Rating> {
     return this.ratingsService.findOne({ where: { id } });
   }
 
   @HttpCode(HttpStatus.OK)
   @Get()
-  findAll() {
+  @ApiOkResponse({
+    description: 'Ratings have been found',
+    type: Rating,
+    isArray: true,
+  })
+  findAll(): Promise<Rating[]> {
     return this.ratingsService.findMany();
   }
 
   @HttpCode(HttpStatus.CREATED)
   @Post()
-  create(@Body() createRatingDto: CreateRatingDto) {
+  @ApiCreatedResponse({
+    description: 'Rating has been created',
+    type: Rating,
+  })
+  @ApiBadRequestResponse({
+    description: 'Invalid data',
+  })
+  create(@Body() createRatingDto: CreateRatingDto): Promise<Rating> {
     return this.ratingsService.create(createRatingDto);
   }
 
   @HttpCode(HttpStatus.OK)
   @Delete(':id')
-  delete(@Param('id') id: string) {
+  @ApiOkResponse({
+    description: 'Rating has been deleted',
+    type: Rating,
+  })
+  @ApiNotFoundResponse({
+    description: 'Rating has not been found',
+  })
+  delete(@Param('id') id: string): Promise<Rating> {
     return this.ratingsService.remove(id);
   }
 }

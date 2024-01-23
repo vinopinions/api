701e1567a3940f83c6a1e8ae9d9438ab5db92f5c
oskar <ooskar@gmx.de>
2024-01-23 11:41:37 +0100

    added utility to clear database

---
+++ test/utils.ts
@@ -0,0 +1,13 @@
+import { INestApplication } from '@nestjs/common';
+import { EntityManager } from 'typeorm';
+
+export const clearDatabase = async (app: INestApplication): Promise<void> => {
+  const entityManager = app.get<EntityManager>(EntityManager);
+  const tableNames = entityManager.connection.entityMetadatas
+    .map((entity) => `"${entity.tableName}"`)
+    .join(', ');
+
+  console.log(tableNames);
+
+  await entityManager.query(`truncate ${tableNames} restart identity cascade;`);
+};

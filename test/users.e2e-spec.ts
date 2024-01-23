0c9354c3103075809097966849a600b5c0d7347c
oskar <ooskar@gmx.de>
2024-01-23 12:38:19 +0100

    added first users tests

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,12 +1,18 @@
+import { faker } from '@faker-js/faker';
 import { HttpStatus, INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
+import {
+  AUTH_LOGIN_ENDPOINT,
+  AUTH_SIGNUP_ENDPOINT,
+} from '../src/auth/auth.controller';
 import { USERS_ENDPOINT } from '../src/users/users.controller';
 import { AppModule } from './../src/app.module';
 import { clearDatabase } from './utils';
 
 describe('UsersController (e2e)', () => {
   let app: INestApplication;
+  let authHeader: object;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -15,6 +21,23 @@ describe('UsersController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+    const accountData = {
+      username: faker.internet.userName(),
+      password: faker.internet.password(),
+    };
+
+    const signUpResponse = await request(app.getHttpServer())
+      .post(AUTH_SIGNUP_ENDPOINT)
+      .send(accountData);
+    if (!(signUpResponse.status == HttpStatus.CREATED)) return;
+
+    const logInResponse = await request(app.getHttpServer())
+      .post(AUTH_LOGIN_ENDPOINT)
+      .send(accountData);
+    if (!(logInResponse.status == HttpStatus.CREATED)) return;
+    authHeader = {
+      Authorization: `Bearer ${logInResponse.body.access_token}`,
+    };
   });
 
   afterEach(async () => {
@@ -28,8 +51,32 @@ describe('UsersController (e2e)', () => {
   describe(USERS_ENDPOINT + ' (GET)', () => {
     it('should exist', () => {
       return request(app.getHttpServer())
-        .post(USERS_ENDPOINT)
+        .get(USERS_ENDPOINT)
         .expect((response) => response.status !== HttpStatus.NOT_FOUND);
+    });
+
+    it(`should return ${HttpStatus.UNAUTHORIZED} without authorization`, async () => {
+      return request(app.getHttpServer())
+        .get(USERS_ENDPOINT)
+        .expect(HttpStatus.UNAUTHORIZED);
+    });
+
+    it(`should return ${HttpStatus.OK} with authorization`, async () => {
+      return request(app.getHttpServer())
+        .get(USERS_ENDPOINT)
+        .set(authHeader)
+        .expect(HttpStatus.OK);
+    });
+
+    it(`should return ${HttpStatus.OK} and  array with length of one with authorization`, async () => {
+      return request(app.getHttpServer())
+        .get(USERS_ENDPOINT)
+        .set(authHeader)
+        .expect(HttpStatus.OK)
+        .expect((res) => {
+          expect(Array.isArray(res.body)).toBe(true);
+          expect((res.body as Array<any>).length).toBe(1);
+        });
     });
   });
 });

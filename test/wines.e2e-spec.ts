eaa23e25f17aec77cfc3463efde2b7f8f6eaf22f
tschokri <layla.shokri@gmx.de>
2024-01-24 21:56:39 +0100

    added some post tests

--- test/wines.e2e-spec.ts
+++ test/wines.e2e-spec.ts
@@ -4,6 +4,7 @@ import { AppModule } from '../src/app.module';
 import { clearDatabase, login } from './utils';
 import { WINES_ENDPOINT } from '../src/wines/wines.controller';
 import request from 'supertest';
+import { CreateWineDto } from '../src/wines/dtos/create-wine.dto';
 
 describe('WinesController (e2e)', () => {
   let app: INestApplication;
@@ -45,6 +46,39 @@ describe('WinesController (e2e)', () => {
         .get(WINES_ENDPOINT)
         .set(authHeader)
         .expect((response) => response.status === HttpStatus.OK);
+    });
+  });
+
+  describe('POST ' + WINES_ENDPOINT, () => {
+    it(`should return ${HttpStatus.CREATED}`, () => {
+      const createWineDto: CreateWineDto = {
+        name: 'Wine',
+        grapeVariety: 'Grapes',
+        heritage: 'Germany',
+        year: 2021,
+        winemakerId: '1',
+        storeIds: ['1'],
+      };
+
+      return request(app.getHttpServer())
+        .post(WINES_ENDPOINT)
+        .set(authHeader)
+        .send(createWineDto)
+        .expect((response) => response.status === HttpStatus.CREATED);
+    });
+
+    it(`should return ${HttpStatus.BAD_REQUEST} when required field is missing`, () => {
+      // name is missing
+      const wineObject = {
+        year: 2020,
+        grapeVariety: 'Grapes',
+        haritage: 'Germany',
+      };
+
+      return request(app.getHttpServer())
+        .post(WINES_ENDPOINT)
+        .send(wineObject)
+        .expect((response) => response.status === HttpStatus.BAD_REQUEST);
     });
   });
 });

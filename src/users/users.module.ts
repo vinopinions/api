71d4c4804c24f5b5c73b485b0e440290e13404a5
oskar <36141470+devskar@users.noreply.github.com>
2024-01-26 20:57:24 +0100

    Update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { RatingsModule } from 'src/ratings/ratings.module';
+import { RatingsModule } from '../ratings/ratings.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), RatingsModule],

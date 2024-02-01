9e5fbbc1d859f110c19b6fb650003de70b250f49
tschokri <layla.shokri@gmx.de>
2024-02-01 16:31:04 +0100

    removed unused import

--- src/wines/entities/wine.entity.ts
+++ src/wines/entities/wine.entity.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsDate, IsString, IsUUID, Matches } from 'class-validator';
+import { IsDate, IsString, IsUUID } from 'class-validator';
 import {
   Column,
   CreateDateColumn,

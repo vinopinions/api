d4bd0c0c698727e3417f2f2a7fd7303516b0a724
oskar <ooskar@gmx.de>
2024-02-04 00:01:49 +0100

    fda

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import packagejson from '../package.json';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log(process.env);
+  console.log(process.env['JWT_PRIVATE_KEY']?.length);
+  console.log(process.env['JWT_PUBLIC_KEY']?.length);
   const app = await NestFactory.create(AppModule);
   app.enableVersioning({
     type: VersioningType.URI,

61eb9bfe24d672d97a3453f640f233399003bf00
oskar <ooskar@gmx.de>
2024-02-11 00:49:53 +0100

    refactor: format

--- release.config.js
+++ release.config.js
@@ -13,12 +13,14 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@codedependant/semantic-release-docker',
     // only create github release on master branch
-    process.env.BRANCH_NAME == 'master' ? [
-      '@semantic-release/github',
-      {
-        addReleases: 'bottom',
-      },
-    ] : [],
+    process.env.BRANCH_NAME == 'master'
+      ? [
+          '@semantic-release/github',
+          {
+            addReleases: 'bottom',
+          },
+        ]
+      : [],
     [
       {
         dockerRegistry: process.env.DOCKER_REGISTRY,

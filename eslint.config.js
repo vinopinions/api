75f213050a087bfa44ec93f517543e4fb3bd0535
oskar <ooskar@gmx.de>
2024-02-11 13:26:11 +0100

    ci: now use flat config with eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,29 @@
-const config = [
-  {
-    parser: '@typescript-eslint/parser',
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+const config = [{
+  languageOptions: {
+    parser: tsParser,
     parserOptions: {
-      project: 'tsconfig.json',
-      tsconfigRootDir: new URL('.', import.meta.url).pathname,
-      sourceType: 'module',
-    },
-    plugins: ['@typescript-eslint/eslint-plugin'],
-    extends: [
-      'plugin:@typescript-eslint/recommended',
-      'plugin:prettier/recommended',
-    ],
-    root: true,
-    env: {
-      node: true,
-      jest: true,
-    },
-    ignorePatterns: ['.eslintrc.js'],
-    rules: {
-      '@typescript-eslint/interface-name-prefix': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
+      ecmaFeatures: { modules: true },
+      ecmaVersion: 'latest',
+      project: './tsconfig.json',
     },
   },
-];
+  plugins: {
+    '@typescript-eslint': ts,
+    ts,
+    prettierPlugin,
+  },
+  rules: {
+    ...ts.configs['eslint-recommended'].rules,
+    ...ts.configs['recommended'].rules,
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+}]
 
 export default config;

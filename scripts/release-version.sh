cfafb7e30b0e13b161ab3eb7b347e4f5499b3223
oskar <ooskar@gmx.de>
2024-02-13 21:38:13 +0100

    test: now

--- scripts/release-version.sh
+++ scripts/release-version.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -x
 
-if [ "$GITHUB_TOKEN" = "$(cat ./secrets.txt)" ]; then
-    echo "Strings are equal."
-else
-    echo "Strings are not equal."
-fi
-# npx semantic-release@$SEMANTIC_RELEASE_VERSION --dry-run | grep 'The next release version is' | awk '{print $NF}'
+MESSAGE=$(npx semantic-release@$SEMANTIC_RELEASE_VERSION --dry-run)
+FORMAT=$(MESSAGE | grep 'The next release version is' | awk '{print $NF}')
+
+echo $FORMAT

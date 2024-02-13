51de271ce7ed50d6112c8239b80b5d14cd02527f
oskar <ooskar@gmx.de>
2024-02-13 21:02:27 +0100

    ci: what now

--- scripts/release-version.sh
+++ scripts/release-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
-export GITHUB_TOKEN=$GITHUB_TOKEN
+export GITHUB_TOKEN=$GITHUB_TOKENP
 export BRANCH_NAME=$BRANCH_NAME
 
 npx semantic-release@$SEMANTIC_RELEASE_VERSION --dry-run | grep 'The next release version is' | awk '{print $NF}'

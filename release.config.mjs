f18ad45c486e47f51fd5839bba58750809a4520f
oskar <ooskar@gmx.de>
2024-02-13 19:17:03 +0100

    fix: update release workflow

--- release.config.mjs
+++ release.config.mjs
@@ -7,16 +7,6 @@ const plugins = [
       npmPublish: false,
     },
   ],
-  [
-    '@codedependant/semantic-release-docker',
-    {
-      dockerRegistry: process.env.DOCKER_REGISTRY,
-      dockerTags: [process.env.DOCKER_IMAGE_TAG, '{{version}}'],
-      dockerImage: process.env.DOCKER_IMAGE_NAME,
-      dockerProject: process.env.DOCKER_PROJECT,
-      dockerFile: 'Dockerfile',
-    },
-  ],
   '@semantic-release/git',
 ];
 

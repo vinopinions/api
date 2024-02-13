3ef89be6f0741aa0a602bd84af8126ca63a30285
oskar <ooskar@gmx.de>
2024-02-13 21:25:09 +0100

    test: maybe i was stupid

--- scripts/release-version.sh
+++ scripts/release-version.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 set -x
 
-echo "GITHUB_TOKEN length = ${#GITHUB_TOKEN}"
-echo "GITHUB_TOKENP length = ${#GITHUB_TOKENP}"
-
-echo "GITHUB_TOKEN 50th letter = ${GITHUB_TOKEN:49:1}"
-echo "GITHUB_TOKENP 50th letter = ${GITHUB_TOKENP:49:1}"
-
-if [ "$GITHUB_TOKEN" = "$GITHUB_TOKENP" ]; then
-    echo "Strings are equal."
-else
-    echo "Strings are not equal."
-fi
-# npx semantic-release@$SEMANTIC_RELEASE_VERSION --dry-run | grep 'The next release version is' | awk '{print $NF}'
+npx semantic-release@$SEMANTIC_RELEASE_VERSION --dry-run | grep 'The next release version is' | awk '{print $NF}'

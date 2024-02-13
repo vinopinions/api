e051b3c6e9d91c43b81ea3baa32d62c3dc6260f3
oskar <ooskar@gmx.de>
2024-02-13 21:21:42 +0100

    test: just an iedea

--- scripts/release-version.sh
+++ scripts/release-version.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -x
 
-echo "GITHUB_TOKEN length = ${#$GITHUB_TOKEN}"
-echo "GITHUB_TOKENP length = ${#$GITHUB_TOKENP}"
+echo "GITHUB_TOKEN length = ${#GITHUB_TOKEN}"
+echo "GITHUB_TOKENP length = ${#GITHUB_TOKENP}"
 
-echo "GITHUB_TOKEN 50th letter = ${$GITHUB_TOKEN:50:51}"
-echo "GITHUB_TOKENP 50th letter = ${$GITHUB_TOKENP:50:51}"
+echo "GITHUB_TOKEN 50th letter = ${GITHUB_TOKEN:49:1}"
+echo "GITHUB_TOKENP 50th letter = ${GITHUB_TOKENP:49:1}"
 
 if [ "$GITHUB_TOKEN" = "$GITHUB_TOKENP" ]; then
     echo "Strings are equal."

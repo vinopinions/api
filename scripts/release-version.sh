46d1c4407e29bc8916f1dda8824afb085eb649f6
oskar <ooskar@gmx.de>
2024-02-14 09:54:40 +0100

    fix: meh

--- scripts/release-version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-
-npx semantic-release@$SEMANTIC_RELEASE_VERSION
-# MESSAGE=$(npx semantic-release@$SEMANTIC_RELEASE_VERSION)
-
-# FORMAT=$($MESSAGE | grep 'The next release version is' | awk '{print $NF}')
-
-# echo $FORMAT

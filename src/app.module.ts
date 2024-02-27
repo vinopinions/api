8279289134692f88260b84c4b1a6ddd7b9bf5a0d
oskar <ooskar@gmx.de>
2024-02-27 18:59:59 +0100

    refactor: update ValidationPipe configuration in app module
    The ValidationPipe in the app module has been updated to include the following configuration:
    - transform: true
    - validationError: { target: false }
    
    This change ensures that the ValidationPipe will automatically transform incoming data and exclude the target object from validation errors.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,13 @@ import { ConfigModule } from '@nestjs/config';
 import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
+import { FeedModule } from './feed/feed.module';
 import { FriendRequestsModule } from './friend-requests/friend-requests.module';
 import { RatingsModule } from './ratings/ratings.module';
 import { StoresModule } from './stores/stores.module';
 import { UsersModule } from './users/users.module';
 import { WinemakersModule } from './winemakers/winemakers.module';
 import { WinesModule } from './wines/wines.module';
-import { FeedModule } from './feed/feed.module';
 
 @Module({
   imports: [
@@ -37,7 +37,10 @@ import { FeedModule } from './feed/feed.module';
     },
     {
       provide: APP_PIPE,
-      useClass: ValidationPipe,
+      useValue: new ValidationPipe({
+        transform: true,
+        validationError: { target: false },
+      }),
     },
   ],
 })

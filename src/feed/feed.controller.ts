d68a23a9dbbc933e4421a2bd26df4417ecfe56d5
oskar <ooskar@gmx.de>
2024-02-27 17:12:05 +0100

    feat: add endpoint to get user feed and implement feed service logic
    - Added a new endpoint in the `FeedController` to get the user's feed.
    - The endpoint is accessible at `/feed` and requires authentication with a bearer token.
    - The endpoint returns an array of `Rating` entities.
    - Implemented the logic in the `FeedService` to retrieve the feed for a user.
    - The feed is retrieved by finding the user's friends and fetching their ratings.
    - The ratings are ordered by creation date in descending order.

--- src/feed/feed.controller.ts
+++ src/feed/feed.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Req } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { AuthenticatedRequest } from '../auth/auth.guard';
+import { Rating } from '../ratings/entities/rating.entity';
+import { FeedService } from './feed.service';
 
-@Controller('feed')
-export class FeedController {}
+const FEED_ENDPOINT_NAME = 'feed';
+export const FEED_ENDPOINT = `/${FEED_ENDPOINT_NAME}`;
+@Controller(FEED_ENDPOINT_NAME)
+@ApiTags(FEED_ENDPOINT_NAME)
+@ApiUnauthorizedResponse({
+  description: 'Not logged in',
+})
+@ApiBearerAuth()
+export class FeedController {
+  constructor(private feedService: FeedService) {}
+
+  @ApiOperation({ summary: 'get your feed' })
+  @HttpCode(HttpStatus.OK)
+  @Get(FEED_ENDPOINT_NAME)
+  @ApiOkResponse({
+    description: 'Feed has been created',
+    type: Rating,
+    isArray: true,
+  })
+  getCurrentUser(@Req() request: AuthenticatedRequest): Promise<Rating[]> {
+    return this.feedService.getFeedForUser(request.user);
+  }
+}
--- src/feed/feed.service.ts
+++ src/feed/feed.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { FindOptionsWhere } from 'typeorm';
+import { Rating } from '../ratings/entities/rating.entity';
+import { RatingsService } from '../ratings/ratings.service';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
-export class FeedService {}
+export class FeedService {
+  constructor(private ratingsService: RatingsService) {}
+
+  async getFeedForUser(user: User): Promise<Rating[]> {
+    const findOptionsWhere: FindOptionsWhere<Rating>[] = user.friends.map(
+      (friend) => {
+        return { id: friend.id };
+      },
+    );
+
+    const ratings: Rating[] = await this.ratingsService.findMany({
+      where: findOptionsWhere,
+      order: { createdAt: 'DESC' },
+    });
+    return ratings;
+  }
+}

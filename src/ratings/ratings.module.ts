fd68e6feb703d8a3de42774da7d78e7c0d8d461b
oskar <ooskar@gmx.de>
2024-03-24 19:35:57 +0100

    fix: import S3Module instead of S3Service

--- src/ratings/ratings.module.ts
+++ src/ratings/ratings.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { S3Service } from '../s3/s3.service';
+import { S3Module } from '../s3/s3.module';
 import { Rating } from './entities/rating.entity';
 import { RatingsController } from './ratings.controller';
 import { RatingsService } from './ratings.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Rating]), S3Service],
+  imports: [TypeOrmModule.forFeature([Rating]), S3Module],
   controllers: [RatingsController],
   providers: [RatingsService],
   exports: [RatingsService],
